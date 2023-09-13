6776eb513aa1a3b27e7805ea2feb38ae42aa8014
Dawoon Jung <tyranno2020@gmail.com>
2023-09-13 23:57:48 +0900

    Create XXX.sol

---
+++ XXX.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: onther
+
+pragma solidity ^0.8.20;
+
+contract onther {
+    string public name = "Onther";
+    
+}
+
+function Add(uint x, uint y) pure returns (uint) 
+{
+    return x + y;
+}
